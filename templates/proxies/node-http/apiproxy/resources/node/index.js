09971c0a4e42481fbff0176f418fa7f2943bd304
Dino Chiesa <dpchiesa@hotmail.com>
2018-03-05 17:51:06 -0800

    new templates for node servers

---
+++ templates/proxies/node-http/apiproxy/resources/node/index.js
@@ -0,0 +1,29 @@
+// index.js
+// ------------------------------------------------------------------
+//
+
+'use strict';
+const fs = require('fs');
+var http = require('http');
+var url = require('url');
+var applications = JSON.parse(fs.readFileSync('applications.json', 'utf-8'));
+
+function requestHandler(request, response) {
+  var queryparams = url.parse(request.url, true).query;
+  var paramA = queryparams.a;
+  response.writeHead(200, {"Content-Type": "application/json"});
+  var payload = {};
+  if (paramA) {
+    payload.results = applications.filter(function(item){ return (item["soa-classification"] === paramA); });
+  }
+  else {
+    payload.error = "you did not supply the required query param";
+  }
+  response.end(JSON.stringify(payload, null, 2) + '\n');
+}
+
+console.log('node.js application starting...');
+var svr = http.createServer(requestHandler);
+svr.listen(9000, function() {
+  console.log('Node HTTP server is listening');
+});
