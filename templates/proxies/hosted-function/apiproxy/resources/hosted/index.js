39ddffe238d5384725f7ca28654227932e3b8889
Dino Chiesa <dchiesa@google.com>
2019-10-15 18:48:18 -0700

    a batch of new and improved templates

--- templates/proxies/hosted-function/apiproxy/resources/hosted/index.js
+++ templates/proxies/hosted-function/apiproxy/resources/hosted/index.js
@@ -1,23 +1,27 @@
-var http = require('http');
-var url = require('url');
+/* jshint esversion: 8 */
+/* global process */
+
+const http = require('http');
+const url = require('url');
 
 console.log('node.js application starting...');
 console.log(process.env);
 
-var svr = http.createServer(function(req, resp) {
-  var query = url.parse(req.url, true).query;
+const svr = http.createServer(function(req, resp) {
+  let query = url.parse(req.url, true).query;
 
   console.log(req.method, req.url);
 
   resp.setHeader("Content-Type", "application/json");
   resp.end(JSON.stringify({
-      date: new Date(),
-      msg: 'Hello, ' + (query.name || 'World') + '!'
+    date: new Date(),
+    inboundUrl : req.url,
+    msg: 'Hello, ' + (query.name || 'World') + '!'
   }));
 });
 
 process.on('exit', function (code) {
-   console.log('Script terminating with code %s', code); 
+   console.log('Script terminating with code %s', code);
 });
 
 process.on('uncaughtException', function (err) {
